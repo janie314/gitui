1853794ace168ae8aad62a6f1b0784889fd0edc0
12ball <pub@janie.page>
2025-03-23 10:43:46 -0500

    Write commit tree to JSON as well

--- lib/git/diagram.py
+++ lib/git/diagram.py
@@ -2,20 +2,35 @@ import os
 import pathlib
 from lib.git.git import git_commits
 from pyvis.network import Network
+import json
 
 
-def git_diagram():
+def write_git_diagram_html():
     commits = git_commits()
     nt = Network("500px", "500px")
     for commit in commits:
-        nt.add_node(commit["hash"], label=commit["short_hash"]+" "+commit["message"])
+        nt.add_node(
+            commit["hash"], label=commit["short_hash"] + " " + commit["message"]
+        )
     for commit in commits:
         if commit["parents"]:
             for parent in commit["parents"]:
                 nt.add_edge(commit["hash"], parent)
-    # nt.add_edge(1, 2)
     script_dir = pathlib.Path(__file__).parent.absolute()
     out_dir = script_dir / "../../out"
     os.makedirs(out_dir, exist_ok=True)
     os.chdir(out_dir)
-    nt.write_html("nx.html", notebook=False)
+    nt.write_html("commits.html", notebook=False)
+
+
+def write_git_diagram_json():
+    commits = git_commits()
+    commits_json = json.dumps(commits, separators=(",", ":"))
+
+    script_dir = pathlib.Path(__file__).parent.absolute()
+    out_dir = script_dir / "../../out"
+    os.makedirs(out_dir, exist_ok=True)
+    os.chdir(out_dir)
+
+    with open("commits.json", "w") as f:
+        f.write(commits_json)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from lib.git.diagram import git_diagram
+from lib.git.diagram import write_git_diagram_html, write_git_diagram_json
 
 
 def main():
-    git_diagram()
+    write_git_diagram_html()
+    write_git_diagram_json()
 
 
 if __name__ == "__main__":
